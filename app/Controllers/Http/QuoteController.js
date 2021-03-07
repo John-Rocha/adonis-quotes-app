c6ddf5825e030ae370c32a4bac0e50eecd8c7a7b
John-Rocha <52468127+John-Rocha@users.noreply.github.com>
2021-03-07 01:11:22 -0300

    Error in render of show

--- app/Controllers/Http/QuoteController.js
+++ app/Controllers/Http/QuoteController.js
@@ -1,58 +1,60 @@
-'use strict'
+"use strict";
 
- const Quote = use('App/Models/Quote')
+const Quote = use("App/Models/Quote");
 
 class QuoteController {
- 
-  async index ({ view }) {
-    const quote = await Quote.all()
-    return view.render('index', {
-      quotes: quote.toJSON()
-    })
+  async index({ view }) {
+    const quote = await Quote.all();
+    return view.render("index", {
+      quotes: quote.toJSON(),
+    });
   }
 
-  async create ({ view }) {
-    return view.render('quotes.create-quote')
+  async create({ view }) {
+    return view.render("quotes.create-quote");
   }
 
-  async store ({ request, auth, response, session }) {
+  async store({ request, auth, response, session }) {
     const quote = await Quote.create({
       user_id: auth.user.id,
       username: auth.user.username,
-      body: request.input('body')
-    })
-    session.flash({ successmessage: 'Quote has been created' })
-    return response.redirect('/')
+      body: request.input("body"),
+    });
+    session.flash({ successmessage: "Quote has been created" });
+    return response.redirect("/");
   }
 
-  async show ({ params, request, response, view }) {
-    const quote = await Quote.find(params.id)
-    return view.render('quotes.view-quote', {
-      quote: quote.toJSON()
-    })
+  async show({ params, request, response, view }) {
+    const quote = await Quote.find(params.id);
+    return view.render("quotes.view-quote", {
+      quote: quote.toJSON(),
+    });
   }
 
-  async edit ({ params, request, response, view }) {
-    const quote = await Quote.find(params.id)
-    return view.render('quotes.edit-quote', {
-      quote: quote.toJSON()
-    })
+  async edit({ params, request, response, view }) {
+    const quote = await Quote.find(params.id);
+    if (!quote) {
+      return response.redirect("/");
+    }
+    return view.render("quotes.edit-quote", {
+      quote: quote.toJSON(),
+    });
   }
 
-  async update ({ params, request, response }) {
-    const quote = await Quote.find(params.id)
-    quote.body = request.input('body')
-    await quote.save()
-    session.flash({ successmessage: 'Quote has been updated' })
-    return response.redirect('/')
+  async update({ params, request, response, session }) {
+    const quote = await Quote.find(params.id);
+    quote.body = request.input("body");
+    await quote.save();
+    session.flash({ successmessage: "Quote has been updated" });
+    return response.redirect("/");
   }
 
-  async destroy ({ params, request, response }) {
-    const quote = await Quote.find(params.id)
-    await quote.delete()
-    session.flash({ successmessage: 'Quote has been deleted' })
-    return response.redirect('/')
+  async destroy({ params, request, response }) {
+    const quote = await Quote.find(params.id);
+    await quote.delete();
+    session.flash({ successmessage: "Quote has been deleted" });
+    return response.redirect("/");
   }
 }
 
-module.exports = QuoteController
+module.exports = QuoteController;
